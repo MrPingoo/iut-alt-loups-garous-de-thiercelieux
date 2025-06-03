3235b989df5267a0a4d0e005975c8983aa6cc8a6
Julian Layen <layenjulian@gmail.com>
2025-06-03 18:18:06 +0200

    feat : add Questions const

--- src/components/Game/ModalQuestion.jsx
+++ src/components/Game/ModalQuestion.jsx
@@ -1,56 +1,5 @@
 import React, { useState } from "react";
-
-// Liste des questions pour les différentes étapes du jeu
-export const QUESTIONS = [
-  {
-    key: "YES_NO",
-    type: "yesNo",
-    question: "La sorcière souhaite-t-elle utiliser sa potion de vie pour ressusciter une personne ?"
-  },
-  {
-    key: "PLAYER_COUNT",
-    type: "selectNumber",
-    question: "Combien de joueurs souhaitez-vous sélectionner ?",
-    options: [1, 2]
-  },
-  {
-    key: "CUPIDON",
-    type: "selectPlayers",
-    question: "Cupidon, choisissez deux joueurs à lier comme amants :",
-    min: 2,
-    max: 2
-  },
-  {
-    key: "CHASSEUR",
-    type: "selectPlayer",
-    question: "Chasseur, quelle personne souhaitez-vous tuer ?"
-  },
-  {
-    key: "LOUPS_GAROUS",
-    type: "selectPlayer",
-    question: "Loups-garous, quelle personne souhaitez-vous tuer ?"
-  },
-  {
-    key: "VOYANTE",
-    type: "selectCard",
-    question: "Voyante, quelle carte souhaitez-vous voir ?"
-  },
-  {
-    key: "VOLEUR",
-    type: "selectPlayer",
-    question: "Voleur, avec qui souhaitez-vous échanger votre carte ?"
-  },
-  {
-    key: "WITCH_KILL",
-    type: "yesNo",
-    question: "La sorcière souhaite-t-elle tuer une personne ?"
-  },
-  {
-    key: "MAIRE",
-    type: "selectPlayer",
-    question: "Qui doit être élu maire ?"
-  }
-];
+import QUESTIONS from "./../../contants/questions";
 
 function ModalQuestion({ players = [], onLoversSelected }) {
     const [isOpen, setIsOpen] = useState(true);
